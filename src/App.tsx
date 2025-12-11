ca19da5cc1c96eef08f994ff031b5bf0fa26867b
jy0813 <busbug3@gmail.com>
2025-12-12 02:06:46 +0900

    feat: provider 적용 및 button variants export 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./components/Header.tsx"
-import Footer from "./components/Footer.tsx"
-import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import { BrowserRouter as Router } from "react-router-dom";
+import { QueryProvider } from "@/app/providers/QueryProvider";
+import { ModalProvider } from "@/app/providers/ModalProvider";
+import Header from "./components/Header.tsx";
+import Footer from "./components/Footer.tsx";
+import PostsManagerPage from "./pages/PostsManagerPage.tsx";
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  )
-}
+    <QueryProvider>
+      <ModalProvider>
+        <Router>
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-grow container mx-auto px-4 py-8">
+              <PostsManagerPage />
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </ModalProvider>
+    </QueryProvider>
+  );
+};
 
-export default App
+export default App;
